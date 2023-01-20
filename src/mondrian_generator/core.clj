49f248b784b23414f55cf0551e9693cafed796fd
Luis Cárcamo <15707135+luis721@users.noreply.github.com>
2023-01-19 22:45:00 -0500

    created basic functions

--- src/mondrian_generator/core.clj
+++ src/mondrian_generator/core.clj
@@ -1,7 +1,40 @@
 (ns mondrian-generator.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn random-color [options]  (rand-nth options))
+
+(defn random-division [options] (rand-nth options))
+
+(defn is-horizontal-division [] (rand-nth [true false]))
+
+(defn random-max-level [options] (rand-nth options))
+
+(defn create-section [top, left, right, bottom, color, level]
+  {:top top
+   :left left
+   :right right
+   :bottom bottom
+   :color color
+   :level level})
+
+(defn create-horizontal-subsections [section division colors]
+  (let [{:keys [top bottom left right level]} section
+        new-level (inc level)
+        line  (+ left (* division (- right left)))]
+    [(create-section
+      top left line bottom (random-color colors) new-level)
+     (create-section
+      top line right bottom (random-color colors) new-level)]))
+
+(defn create-vertical-subsections [section division colors]
+  (let [{:keys [top bottom left right level]} section
+        new-level (inc level)
+        line  (+ left (* division (- right left)))]
+    [(create-section top left line bottom (random-color colors) new-level)
+     (create-section top line right bottom (random-color colors) new-level)]))
+
+
+(defn split-section [section division colors]
+  (if (is-horizontal-division)
+    (create-horizontal-subsections section division colors)
+    (create-vertical-subsections section division colors)))
